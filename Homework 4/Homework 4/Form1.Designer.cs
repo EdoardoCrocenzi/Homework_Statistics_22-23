d40baa79384270c8aa82f17c8ea44fda0eedd7c0
DragonNest <71124432+EdoardoCrocenzi@users.noreply.github.com>
2022-10-26 12:44:25 +0200

    Add files via upload

--- Homework 4/Homework 4/Form1.Designer.cs
+++ Homework 4/Homework 4/Form1.Designer.cs
@@ -30,43 +30,48 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(384, 350);
+            this.pictureBox1.Size = new System.Drawing.Size(609, 463);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(665, 12);
+            this.button1.Location = new System.Drawing.Point(12, 499);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 66);
+            this.button1.Size = new System.Drawing.Size(93, 57);
             this.button1.TabIndex = 1;
             this.button1.Text = "Absolute Frequency";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(426, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(213, 350);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(665, 155);
+            this.button2.Location = new System.Drawing.Point(194, 499);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 66);
+            this.button2.Size = new System.Drawing.Size(91, 57);
             this.button2.TabIndex = 5;
             this.button2.Text = "Relative frequency";
             this.button2.UseVisualStyleBackColor = true;
@@ -74,28 +79,136 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(665, 296);
+            this.button3.Location = new System.Drawing.Point(385, 499);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(123, 66);
+            this.button3.Size = new System.Drawing.Size(96, 57);
             this.button3.TabIndex = 6;
             this.button3.Text = "Normalized Frequency";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1165, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Probability";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1147, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Number of Trials";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1127, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Number of Trajectories";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1127, 45);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(161, 56);
+            this.trackBar1.TabIndex = 14;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(1127, 188);
+            this.trackBar2.Maximum = 250;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(161, 56);
+            this.trackBar2.TabIndex = 15;
+            this.trackBar2.Value = 100;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(1127, 338);
+            this.trackBar3.Maximum = 250;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(161, 56);
+            this.trackBar3.TabIndex = 16;
+            this.trackBar3.Value = 10;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1197, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "50";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1197, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "100";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1197, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "10";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(645, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(476, 463);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1316, 618);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,8 +216,17 @@
 
         private PictureBox pictureBox1;
         private Button button1;
-        private RichTextBox richTextBox1;
         private Button button2;
         private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private TrackBar trackBar3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private PictureBox pictureBox2;
     }
 }
--- Homework 4/Homework 4/Form1.cs
+++ Homework 4/Homework 4/Form1.cs
@@ -1,16 +1,23 @@
+using NUnit.Framework.Constraints;
+
 namespace Homework_4
 {
     public partial class Form1 : Form
     {
         Bitmap bmp;
+        Bitmap bmp2;
         Graphics g;
+        Graphics g2;
         Random r = new Random();
         Pen p = new Pen(Brushes.Gray);
         Pen p2 = new Pen(Brushes.Red);
         Pen p3 = new Pen(Brushes.Green);
         Pen p4 = new Pen(Brushes.Blue);
-        int absolute_frequency = 0;
-        int absolute_tentatives = 0;
+
+        int probability = 50;
+        int TrialsCount = 100;
+        int Trajectories = 10;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,11 +28,10 @@ namespace Homework_4
             bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
             g = Graphics.FromImage(bmp);
             g.Clear(Color.White);
-            this.richTextBox1.Clear();
 
-
-            int TrialsCount = 100;
-            int Trajectories = 10;
+            bmp2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            g2 = Graphics.FromImage(bmp2);
+            g2.Clear(Color.White);
 
             double minX = 0;
             double minY = 0;
@@ -35,33 +41,67 @@ namespace Homework_4
 
             Rectangle VirtualWindow = new Rectangle(20, 20, bmp.Width - 40, bmp.Height - 40);
             g.DrawRectangle(p, VirtualWindow);
-            for(int i = 1; i <= Trajectories; i++)
+
+            Rectangle VirtualWindow2 = new Rectangle(20, 20, bmp2.Width - 40, bmp2.Height - 40);
+            g2.DrawRectangle(p, VirtualWindow2);
+
+            List<Point> Final_Point = new List<Point>();
+            for (int i = 1; i <= Trajectories; i++)
             {
                 List<Point> Absolute_Frequency = new List<Point>();
                 int success = 0;
-                int unsuccess = 0;
                 for (int j = 0; j < TrialsCount; j++)
                 {
-                    int uniform = r.Next(2);
-                    if (uniform == 0)
+                    int uniform = r.Next(100);
+                    if (uniform <= probability)
                     {
                         success++;
-                        this.richTextBox1.AppendText("Success: " + success.ToString() + " Unsuccess: " + unsuccess.ToString() + "\n");
-                        this.richTextBox1.ScrollToCaret();
-                    }
-                    else
-                    {
-                        unsuccess++;
-                        this.richTextBox1.AppendText("Success: " + success.ToString() + " Unsuccess: " + unsuccess.ToString() + "\n");
-                        this.richTextBox1.ScrollToCaret();
                     }
                     int xAbsoluteDevice = FromXRealToXVirtual(j, minX, maxX, VirtualWindow.Left, VirtualWindow.Width);
                     int yAbsoluteDevice = FromYRealToYVirtual(success, minY, maxY, VirtualWindow.Top, VirtualWindow.Height);
                     Absolute_Frequency.Add(new Point(xAbsoluteDevice, yAbsoluteDevice));
+                    if (j == TrialsCount - 1)
+                    {
+                        Final_Point.Add(new Point(xAbsoluteDevice, yAbsoluteDevice));
+                    }
                 }
                 g.DrawLines(p2, Absolute_Frequency.ToArray());
             }
+            Dictionary<Point, int> Histograms = new Dictionary<Point, int>();
+
+            foreach (Point punto in Final_Point)
+            {
+                bool interval = false;
+                foreach (KeyValuePair<Point, int> pair in Histograms)
+                {
+                    int v = Math.Abs(punto.Y - pair.Key.Y);
+                    if (v <= 5)
+                    {
+                        Histograms[pair.Key] += 5;
+                        interval = true;
+                        break;
+                    }
+                }
+                if (interval == false)
+                {
+                    Histograms.Add(punto, 5);
+                }
+
+            }
+            foreach (KeyValuePair<Point, int> histogram in Histograms)
+            {
+                int Y = histogram.Value;
+                if (Y > VirtualWindow2.Width)
+                {
+                    Y = VirtualWindow2.Width;
+                }
+                Rectangle istogramma = new Rectangle(VirtualWindow2.Left, histogram.Key.Y, Y, 5);
+                g2.FillRectangle(Brushes.Red, istogramma);
+                g2.DrawRectangle(p, istogramma);
+
+            }
             this.pictureBox1.Image = bmp;
+            this.pictureBox2.Image = bmp2;
         }
         private int FromXRealToXVirtual(double X, double minX, double maxX, int left, int W)
         {
@@ -78,7 +118,7 @@ namespace Homework_4
             {
                 return 0;
             }
-            int res = (int)((int)top + H * (Y - minY) / (maxY - minY));
+            int res = (int)((int)top + H - H * (Y - minY) / (maxY - minY));
             return res;
         }
 
@@ -87,11 +127,10 @@ namespace Homework_4
             bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
             g = Graphics.FromImage(bmp);
             g.Clear(Color.White);
-            this.richTextBox1.Clear();
 
-
-            int TrialsCount = 100;
-            int Trajectories = 10;
+            bmp2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            g2 = Graphics.FromImage(bmp2);
+            g2.Clear(Color.White);
 
             double minX = 0;
             double minY = 0;
@@ -101,36 +140,69 @@ namespace Homework_4
 
             Rectangle VirtualWindow = new Rectangle(20, 20, bmp.Width - 40, bmp.Height - 40);
             g.DrawRectangle(p, VirtualWindow);
+
+            Rectangle VirtualWindow2 = new Rectangle(20, 20, bmp2.Width - 40, bmp2.Height - 40);
+            g2.DrawRectangle(p, VirtualWindow2);
+
+            List<Point> Final_Point = new List<Point>();
             for (int i = 1; i <= Trajectories; i++)
             {
                 List<Point> Relative_Frequency = new List<Point>();
                 int success = 0;
-                int unsuccess = 0;
                 for (int j = 0; j < TrialsCount; j++)
                 {
-                    int uniform = r.Next(2);
-                    if (uniform == 0)
+                    int uniform = r.Next(100);
+                    if (uniform <= probability)
                     {
                         success++;
-                        double rel_freq = success *  TrialsCount/ (j + 1);
-                        this.richTextBox1.AppendText("Success: " + success.ToString() + " Unsuccess: " + unsuccess.ToString() + " Relative Frequency: " + rel_freq.ToString() +  "\n");
-                        this.richTextBox1.ScrollToCaret();
-                    }
-                    else
-                    {
-                        unsuccess++;
-                        this.richTextBox1.AppendText("Success: " + success.ToString() + " Unsuccess: " + unsuccess.ToString() + "\n");
-                        this.richTextBox1.ScrollToCaret();
                     }
                     double Y = success * TrialsCount / (j + 1);
                     int xRelativeDevice = FromXRealToXVirtual(j, minX, maxX, VirtualWindow.Left, VirtualWindow.Width);
                     int yRelativeDevice = FromYRealToYVirtual(Y, minY, maxY, VirtualWindow.Top, VirtualWindow.Height);
                     Relative_Frequency.Add(new Point(xRelativeDevice, yRelativeDevice));
 
+                    if (j == TrialsCount - 1)
+                    {
+                        Final_Point.Add(new Point(xRelativeDevice, yRelativeDevice));
+                    }
                 }
                 g.DrawLines(p3, Relative_Frequency.ToArray());
             }
+            Dictionary<Point, int> Histograms = new Dictionary<Point, int>();
+
+            foreach (Point punto in Final_Point)
+            {
+                bool interval = false;
+                foreach (KeyValuePair<Point, int> pair in Histograms)
+                {
+                    int v = Math.Abs(punto.Y - pair.Key.Y);
+                    if (v <= 5)
+                    {
+                        Histograms[pair.Key] += 5;
+                        interval = true;
+                        break;
+                    }
+                }
+                if (interval == false)
+                {
+                    Histograms.Add(punto, 5);
+                }
+
+            }
+            foreach (KeyValuePair<Point, int> histogram in Histograms)
+            {
+                int Y = histogram.Value;
+                if (Y > VirtualWindow2.Width)
+                {
+                    Y = VirtualWindow2.Width;
+                }
+                Rectangle istogramma = new Rectangle(VirtualWindow2.Left, histogram.Key.Y, Y, 5);
+                g2.FillRectangle(Brushes.Green, istogramma);
+                g2.DrawRectangle(p, istogramma);
+
+            }
             this.pictureBox1.Image = bmp;
+            this.pictureBox2.Image = bmp2;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -138,11 +210,10 @@ namespace Homework_4
             bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
             g = Graphics.FromImage(bmp);
             g.Clear(Color.White);
-            this.richTextBox1.Clear();
 
-
-            int TrialsCount = 100;
-            int Trajectories = 10;
+            bmp2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            g2 = Graphics.FromImage(bmp2);
+            g2.Clear(Color.White);
 
             double minX = 0;
             double minY = 0;
@@ -152,37 +223,97 @@ namespace Homework_4
 
             Rectangle VirtualWindow = new Rectangle(20, 20, bmp.Width - 40, bmp.Height - 40);
             g.DrawRectangle(p, VirtualWindow);
+
+            Rectangle VirtualWindow2 = new Rectangle(20, 20, bmp2.Width - 40, bmp2.Height - 40);
+            g2.DrawRectangle(p, VirtualWindow2);
+
+            List<Point> Final_Point = new List<Point>();
             for (int i = 1; i <= Trajectories; i++)
             {
                 List<Point> Normalized_Frequency = new List<Point>();
                 int success = 0;
-                int unsuccess = 0;
                 for (int j = 0; j < TrialsCount; j++)
                 {
-                    int uniform = r.Next(2);
-                    if (uniform == 0)
+                    int uniform = r.Next(100);
+                    if (uniform <= probability)
                     {
                         success++;
-                        double norm_freq = success * TrialsCount / (Math.Sqrt(j + 1));
-                        this.richTextBox1.AppendText("Success: " + success.ToString() + " Unsuccess: " + unsuccess.ToString() + "Normalized Frequency: " + norm_freq.ToString() + "\n");
-                        this.richTextBox1.ScrollToCaret();
                     }
-                    else
-                    {
-                        unsuccess++;
-                        this.richTextBox1.AppendText("Success: " + success.ToString() + " Unsuccess: " + unsuccess.ToString() + "\n");
-                        this.richTextBox1.ScrollToCaret();
-                    }
-
                     double nY = success * Math.Sqrt(TrialsCount) / (Math.Sqrt(j + 1));
                     int xNormalizedDevice = FromXRealToXVirtual(j, minX, maxX, VirtualWindow.Left, VirtualWindow.Width);
                     int yNormalizedeDevice = FromYRealToYVirtual(nY, minY, maxY, VirtualWindow.Top, VirtualWindow.Height);
                     Normalized_Frequency.Add(new Point(xNormalizedDevice, yNormalizedeDevice));
 
+                    if (j == TrialsCount - 1)
+                    {
+                        Final_Point.Add(new Point(xNormalizedDevice, yNormalizedeDevice));
+                    }
                 }
                 g.DrawLines(p4, Normalized_Frequency.ToArray());
             }
+            Dictionary<Point, int> Histograms = new Dictionary<Point, int>();
+
+            foreach (Point punto in Final_Point)
+            {
+                bool interval = false;
+                foreach (KeyValuePair<Point, int> pair in Histograms)
+                {
+                    int v = Math.Abs(punto.Y - pair.Key.Y);
+                    if (v <= 5)
+                    {
+                        Histograms[pair.Key] += 5;
+                        interval = true;
+                        break;
+                    }
+                }
+                if (interval == false)
+                {
+                    Histograms.Add(punto, 5);
+                }
+
+            }
+            foreach (KeyValuePair<Point, int> histogram in Histograms)
+            {
+                int Y = histogram.Value;
+                if (Y > VirtualWindow2.Width)
+                {
+                    Y = VirtualWindow2.Width;
+                }
+                Rectangle istogramma = new Rectangle(VirtualWindow2.Left, histogram.Key.Y, Y, 5);
+                g2.FillRectangle(Brushes.Blue, istogramma);
+                g2.DrawRectangle(p, istogramma);
+
+            }
             this.pictureBox1.Image = bmp;
+            this.pictureBox2.Image = bmp2;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.label4.Text = this.trackBar1.Value.ToString();
+            probability = this.trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            this.label5.Text = this.trackBar2.Value.ToString();
+            TrialsCount = this.trackBar2.Value;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            this.label6.Text = this.trackBar3.Value.ToString();
+            Trajectories = this.trackBar3.Value;
         }
     }
 }
